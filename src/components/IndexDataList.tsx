f53ef0bd1d38d4c9a27297b78517e6236975d0a9
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-02-22 16:59:50 -0400

    add back in pagination, only partially working

--- src/components/IndexDataList.tsx
+++ src/components/IndexDataList.tsx
@@ -1,5 +1,5 @@
 import DataItemDialog from "components/DataItemDialog";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PageScrollSpy from "components/PageScrollSpy";
 import DatasetIndex, {
   getDatasetGetAllMethod,
@@ -9,6 +9,7 @@ import { DatasetsAvailable } from "types/dataset-index-type";
 
 import { useAppDispatch } from "../app/hooks";
 import { apiSlice } from "../services/apiSlice";
+import { createSelector } from "@reduxjs/toolkit";
 interface IndexDataListProps {
   datasetId: DatasetsAvailable;
 }
@@ -26,21 +27,45 @@ function IndexDataList({ datasetId }: IndexDataListProps) {
     dispatch(apiSlice.endpoints[datasetIndexName].initiate());
   }, [datasetId, dispatch]);
 
+  // adapted from here: https://redux.js.org/tutorials/essentials/part-8-rtk-query-advanced#selecting-values-from-results
+
+  // @TODO: this is only sort of working now that I added in the
+  // imperative dispatch in the hook above.
+  const selectPaginatedDataItems = useMemo(() => {
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res?: typeof data) => res,
+      (res: typeof data, lastIndex: number) => lastIndex,
+      (res, lastIndex) => (res ? res.slice(0, lastIndex) : [])
+    );
+  }, []);
+
+  const { paginatedDataItems } = DatasetIndex[datasetId].getAll(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      paginatedDataItems: selectPaginatedDataItems(
+        result.data || [],
+        lastIndex
+      ),
+    }),
+  });
+
   return (
     <div style={{ textAlign: "left", fontSize: "9px" }}>
       {data && <h2>Current Dataset: {DatasetIndex[datasetId].title}</h2>}
       {data && (
         <h4>
           <span>Total Num Items: {data.length}</span>/
-          {data && <span> Current Num Items: {data.length}</span>}
+          {data && <span> Current Num Items: {paginatedDataItems.length}</span>}
         </h4>
       )}
       {isLoading && <div>Loading...</div>}
       {isFetching && <div>Fetching...</div>}
       {/* {isError && <div>isError...</div>}
       {error && <div>error...</div>} */}
-      {(data as typeof data) &&
-        data?.map((item: any, index: number) => (
+      {(paginatedDataItems as typeof data) &&
+        paginatedDataItems?.map((item: any, index: number) => (
           <DataItemDialog key={index} dataItem={item} datasetId={datasetId} />
         ))}
       {error && (
