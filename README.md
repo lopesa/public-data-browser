ef235d4baa08510f53ccd678d3925d1db2f4081f
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-03-26 14:50:27 -0400

    add (swap) favicon, move type imports into dev deps

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Getting Started with Create React App
+## Depends on Public Data Browser Backend
+
+in a separate repo
+
+---
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) TS template.
 
--- src/components/PDB_Checkbox.tsx
+++ src/components/PDB_Checkbox.tsx
@@ -1,29 +1,6 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
-import styled from "styled-components";
-import { blackA, violet } from "@radix-ui/colors";
 import styles from "styles/PDBCheckbox.module.scss";
-
-const CheckboxRoot = styled(Checkbox.Root)`
-  background-color: white;
-  width: 20px;
-  height: 20px;
-  border-radius: 2px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0 2px 10px ${blackA.blackA7};
-  &:hover {
-    background-color: ${violet.violet3};
-  }
-  &:focus {
-    box-shadow: 0 0 0 2px black;
-  }
-`;
-
-const CheckboxIndicator = styled(Checkbox.Indicator)`
-  color: var(--violet11);
-`;
 
 interface PDBCheckboxProps {
   onCheckedChange: (checked: boolean) => void;
@@ -33,15 +10,15 @@ interface PDBCheckboxProps {
 const PDBCheckbox = ({ onCheckedChange, label }: PDBCheckboxProps) => (
   <form>
     <div style={{ display: "flex", alignItems: "center" }}>
-      <CheckboxRoot
+      <Checkbox.Root
         onCheckedChange={onCheckedChange}
         className={styles.CheckboxRoot}
         id="c1"
       >
-        <CheckboxIndicator className={styles.CheckboxIndicator}>
+        <Checkbox.Indicator className={styles.CheckboxIndicator}>
           <CheckIcon />
-        </CheckboxIndicator>
-      </CheckboxRoot>
+        </Checkbox.Indicator>
+      </Checkbox.Root>
       <label className={styles.CheckboxLabel} htmlFor="c1">
         {label}
       </label>
