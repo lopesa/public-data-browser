4b22e7592de8ee1a19858789d25e8509c6e8d91c
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-04-06 10:43:39 -0400

    update ABOUT copy

--- src/routes/About.route.tsx
+++ src/routes/About.route.tsx
@@ -6,27 +6,52 @@ export default function About() {
       <h3>About</h3>
       <p>
         The primary driver for this project is that I&rsquo;m an Engineer who
-        needed a side project. For more of a look at what I used technically
-        please check out blog posts [tbd]
+        needed a side project. So I took this opportunity to build a full stack
+        app from greenfield, taking care to choose tech pieces I wanted to work
+        with/master. So it includes:
+        <ul>
+          <li>Docker (compose)</li>
+          <li>Express</li>
+          <li>Prisma</li>
+          <li>MySql</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>redux-toolkit, rtk query</li>
+          <li>radix-ui</li>
+        </ul>
       </p>
-      <p>
-        Core functionality for now should be to take in any JSON dataset and
-        present it in a readable way, especially to quickly preview data sets
-        that may be included. It should be built in a modular way so those core
-        functions can expand easily. (Import other than JSON, do more with the
-        datasets, etc.)
-      </p>
-      <p>
+      {/* <p>
         It's interesting how much data there is publically available and I
         wanted a single place to browse though it, without the mental overhead
-        of switching presentations often.
+        of switching presentations often. In a way though, this app represents
+        the core of many apps.
+      </p> */}
+      <p>
+        Core functionality for the app for now should be to take in any JSON
+        dataset and present it in a consistent, readable way, especially to
+        quickly preview data sets that may be included. It should be built in a
+        modular way so those core functions can expand easily. (Import other
+        than JSON, do more with the datasets, etc.)
       </p>
       <p>
-        In a way though, this app represents the core of most apps. This
-        represents one of my main 2 greenfield stacks to build an app. This one
-        beiong redux-ey and the other being next.js.
+        Funny sidenote, on March 30, 2023 data.gov did a big redesign, making
+        their interface to browse this data much better as well as (afaik)
+        taking away the raw json data offering, which underlies much of this at
+        mvp. While this project aims to ingest any data, honestly the main data
+        source was to be, at least at first, data.gov data. I am thinking to
+        take it as a complement to my design sense that I saw that their
+        browsing mechanism was easily improvable :) , otherwise I'm just kind of
+        bummed I can't exactly release this at a more public url as more of an
+        actual thing, like I was going to. I'm lucky my original idea was to not
+        even ingest their json data into my db, and just work with files, so I
+        had the json files of a couple of the departments saved from before
+        their redesign. Anyway, the primary reason for this project stands
+        accomplished which was for me to build it. I am however releasing it now
+        as an unpolished prototype. At the very least the data.gov data will
+        become stale, so please actually use this at your own risk. Or don't. No
+        guarantees what will become of it.
       </p>
-
+      <p>the code for it is here:</p>
       {/* <p className={styles.Bold}>
         • This is piecing together a lot of awesome open source tools and
         libraries, and that's mostly all it is. The code for this available
